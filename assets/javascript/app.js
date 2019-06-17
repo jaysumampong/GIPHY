0d101e7b110438e0b7ef54bae8c7713e803762c4
Meng Hao <jaysumampong@gmail.com>
2019-06-16 19:16:49 -0600

    updated psuedo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
-// Set event listeners
+// Set event listeners to all button elements
 // Set variables
+    // Construct a URL to search GIPHY
 // Set Functions
+    // Set up AJAX request 
+    // get response and console log response and additional parameters
+    // store data from AJAX request into a variable
+    // create for loop for the data
+    // Creating and storing a div tag
+    // Creating a paragraph tag with the result item's rating
+    // Creating and storing an image tag
+    // Setting the src attribute of the image to a property pulled off the result item
+    // Appending the paragraph and image tag to the animalDiv
+    // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+
+//Set up functions to pauce gifs
+    // 
