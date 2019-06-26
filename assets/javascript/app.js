8b3b5c65d0bc514704797cd3c72b1707be46610f
Meng Hao <jaysumampong@gmail.com>
2019-06-25 18:12:23 -0600

    fixed a few functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     topics = ["Avengers", "Kill Bill", "Harry Potter", "Avatar", "Lord of The Rings"];
-    // Having a hard time trying to get the buttons to perform ajax calls
+    // function to display gifs
     function callAjax(){
-        let movies = $(this).attr("data-movie");
+        let movies = $(this).attr("data-name");
         let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
             movies + "&api_key=bskkca2lWVA6r2pYHlMYhl8IK23EVw4F&limit=10";
 
@@ -15,7 +15,7 @@ $(document).ready(function(){
             let results = response.data;
                 for (let i = 0; i < results.length; i++){
                     if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
-                        let movieDiv = $("#gif-buttons");
+                        let movieDiv = $("<div class='movie'>");
                         let p = $("<p>").text("Rating: " + results[i].rating);
                         let movieImage = $("<img>");
                         movieImage.attr("id", "giphy");
@@ -25,45 +25,46 @@ $(document).ready(function(){
                         movieImage.attr("data-state", "still"); 
                         movieDiv.append(p);
                         movieDiv.append(movieImage);
+                        $("#gifs").prepend(movieDiv);
                 }
             }
         });
-    } callAjax();
+    }
     
     // this displays the initial button when loading the page
     function initialDisplay() {
         $("#gif-buttons").empty();
         for (let i = 0; i < topics.length; i++) {
             let initialButton = $("<button>");
-            initialButton.addClass("button-color");
-            initialButton.attr("data-movie", topics[i]);
+            initialButton.addClass("movie");
+            initialButton.attr("data-name", topics[i]);
             initialButton.text(topics[i]);
             $("#gif-buttons").prepend(initialButton);
         };
-    } initialDisplay();
-  
+    }; 
+    
     // creates new gif buttons
-   $("#add-submit").on("click", function(event){
-       event.preventDefault();
-       let newButtonsTask = $("#add-gif").val().trim();
-       let buttonItem = $("<button>");
-       
-       buttonItem.attr("data-movie");
-       buttonItem.addClass("button-color");
-       buttonItem.text(newButtonsTask);
-       buttonItem = buttonItem.prepend(newButtonsTask);
-       $("#gif-buttons").append(buttonItem);
-       $("add-gif").val("");
-   });
+    $("#add-submit").on("click", function(event){
+        event.preventDefault();
+        let newButtonsTask = $("#add-gif").val().trim();
+        topics.push(newButtonsTask);
+        initialDisplay();
+    });
+    
+    initialDisplay();
+    
     //event listener that changes gifs from animated to still    
-   $(document).on("click", "#giphy", function(){
-    var state = $(this).attr('data-state');
-    if ( state == 'still'){
-        $(this).attr('src', $(this).data('animate'));
-        $(this).attr('data-state', 'animate');
-    }else{
-        $(this).attr('src', $(this).data('still'));
-        $(this).attr('data-state', 'still');
-    }
-});
+    $(document).on("click", ".movie", callAjax);
+    
+    $(document).on("click", "#giphy", function(){
+        var state = $(this).attr('data-state');
+        if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+        
+    });
 });
